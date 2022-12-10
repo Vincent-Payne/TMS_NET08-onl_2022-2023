aa968c7c509d68f7a609da9df2550ae9d4c11e32
Vincent-Payne <mr.vinpay@gmail.com>
2022-12-10 11:26:22 +0300

    Added new lab - 02_07.12.2022_Homework

---
+++ 02_07.12.2022_Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_07._12._2022_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
