3d18162f2e785b238e3c26a41ecb70c08bcdf636
119345641+Vincent-Payne@users.noreply.github.com <mr.vinpay@gmail.com>
2022-12-21 22:04:14 +0300

    Added new lab

---
+++ 07_Interfaces_Practise_21.12.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07_21._12._2022_Practise
+{
+    internal class Program
+    {
+        public interface ILaunchStation
+        {
+            void Start(string transport)
+            {
+                Console.WriteLine($"I'm flying on {transport}");
+            }
+        }
+        public class Transport: ILaunchStation
+        {
+            string transport;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
