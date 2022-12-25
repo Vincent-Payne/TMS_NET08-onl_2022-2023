4e05aa69815891f780fc51339fb29d62c118bd01
Pavel Dyachuk <mr.vinpay@gmail.com>
2022-12-25 10:49:58 +0300

    Added Practice Lab 21.12.2022

--- 07_Interfaces_Practise_21.12.2022/Program.cs
+++ 07_Interfaces_Practise_21.12.2022/Program.cs
@@ -2,22 +2,63 @@
 {
     internal class Program
     {
+        /*Описать сисстему старотовой установки которая запускает вас в полёт на каком-то объекте.
+        В процессе полёта необходимо вывести сообщение "Я лечу на {название_объекта}"
+        Объекты в стартовую установку передать в конструкторе*/
         public interface ILaunchStation
         {
-            void Start(string transport)
+            string Launch();
+        }
+        public class Broomstick : ILaunchStation
+        {
+            public string Launch()
             {
-                Console.WriteLine($"I'm flying on {transport}");
+                return "Broomstick";
             }
         }
-        public class Transport: ILaunchStation
+        public class Wings : ILaunchStation
         {
-            string transport;
+           public string Launch()
+            {
+                return "Wings";
+            }
+                
+        }
+        public class MagicCarpet : ILaunchStation
+        {
+            public string Launch() 
+            {
+                return "Magic Carpet";
+            }
 
         }
-        
+
+        class LaunchStation
+        {
+            public ILaunchStation ilaunch_station;
+            public LaunchStation(ILaunchStation ils)
+            {
+                ilaunch_station = ils;
+            }
+            public string BeginFlight() 
+            {
+                return ilaunch_station.Launch();
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+
+            Broomstick broomstick = new Broomstick();
+            MagicCarpet magic_carpet = new MagicCarpet();
+            Wings wings = new Wings();
+            LaunchStation launchStation = new LaunchStation(broomstick);
+            LaunchStation launchStation1 = new LaunchStation(magic_carpet);
+            LaunchStation launchStation2 = new LaunchStation(wings);
+
+            Console.WriteLine($"I'm flying on {launchStation.BeginFlight()}");
+            Console.WriteLine($"I'm flying on {launchStation1.BeginFlight()}");
+            Console.WriteLine($"I'm flying on {launchStation2.BeginFlight()}");
         }
     }
 }
