3ec191754e6213bf7c85925f15b10dc6de11a29c
Pavel Dyachuk <mr.vinpay@gmail.com>
2022-12-25 19:52:57 +0300

    minor changes

---
+++ 07_Interfaces_Homework_21.12.2022/Task_2_Employees/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task_2
+{
+    
+/*2. Создать классы "Директор", "Рабочий", "Бухгалтер". 
+Реализовать интерфейс с методом, который печатает название должности и имплементировать этот метод в созданные классы.
+*/
+    internal class Program
+    {
+        public interface IEmployee
+        {
+            string GetInfo();
+        }
+
+        public class Director : IEmployee
+        {
+            string jod_title = "Director";
+            public string GetInfo()
+            {
+                return jod_title;
+            }
+        }
+
+        public class Accountant : IEmployee
+        {
+            string jod_title = "Accountant";
+            public string GetInfo()
+            {
+                return jod_title;
+            }
+        }
+
+        public class Worker : IEmployee
+        {
+            string jod_title = "Worker";
+            public string GetInfo()
+            {
+                return jod_title;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           Director director = new Director();
+           Accountant accountant = new Accountant();
+           Worker worker = new Worker();
+           Console.WriteLine($"{director.GetInfo()} ");
+           Console.WriteLine($"{accountant.GetInfo()} ");
+           Console.WriteLine($"{worker.GetInfo()} ");
+        }
+    }
+}
